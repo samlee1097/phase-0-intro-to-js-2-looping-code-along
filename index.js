40e905629a3736a835acddbb69015eb04439b390
Samuel <samlee1097@yahoo.com>
2021-08-03 02:24:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const newGifts = [];
+    for(let counter = 0; counter < names.length; counter++){ //Iterates equal to the number of names in array
+        newGifts[counter] = `Thank you, ${names[counter]}, for the wonderful ${event} gift!`;
+    }
+    return newGifts;
+};
+
+function countDown(number){
+    let i = 0;
+    while(i <= number){ //Set equal to 0 in order to return 0 as well
+        console.log(number);
+        number--;
+    }
+};
